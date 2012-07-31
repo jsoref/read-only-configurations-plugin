d17f988d22f3d5e6b05b249dcfc3d281d6943e6b
lvotypko <lvotypko@redhat.com>
2012-07-31 14:54:31 +0200

    Replace permission EXTENDED_READ by READ

--- src/main/java/org/jenkinsci/plugins/readonly/JobConfiguration.java
+++ src/main/java/org/jenkinsci/plugins/readonly/JobConfiguration.java
@@ -1,13 +1,21 @@
 package org.jenkinsci.plugins.readonly;
 
 import hudson.model.AbstractProject;
+import hudson.model.Job;
 import hudson.model.ProminentProjectAction;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
+import jenkins.model.Jenkins;
+import org.apache.commons.jelly.JellyContext;
 import org.apache.commons.jelly.JellyException;
 import org.apache.commons.jelly.Script;
 import org.kohsuke.stapler.MetaClass;
@@ -18,7 +26,9 @@ import org.kohsuke.stapler.WebApp;
 import org.kohsuke.stapler.bind.JavaScriptMethod;
 import org.kohsuke.stapler.jelly.DefaultScriptInvoker;      
 import org.kohsuke.stapler.jelly.HTMLWriterOutput;
+import org.kohsuke.stapler.jelly.JellyClassLoaderTearOff;
 import org.kohsuke.stapler.jelly.JellyClassTearOff;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 /**
@@ -34,6 +44,26 @@ public class JobConfiguration implements ProminentProjectAction {
         this.project = project;
         
     }
+    
+    /**
+     * Compile script with a context for Job class
+     * 
+     * @return compiled script
+     */
+    public Script compileScript() {
+        Script result = null;
+        try {
+            MetaClass c = WebApp.getCurrent().getMetaClass(Jenkins.getInstance().getClass());
+            JellyContext context = new JellyClassLoaderTearOff(c.classLoader).createContext();
+            StringReader buffer = new StringReader(getConfigContent());
+            InputSource source = new InputSource(buffer);
+            source.setSystemId(JenkinsConfiguration.class.getResource("JobConfiguration").toString());
+            result = context.compileScript(source);
+        } catch (Exception ex) {
+            Logger.getLogger(JobConfiguration.class.getName()).log(Level.WARNING, "Read-only configuration plugin failed to compile script", ex);
+        }
+        return result;
+    }
 
     public String getIconFileName() {
         return "search.png";
@@ -47,6 +77,26 @@ public class JobConfiguration implements ProminentProjectAction {
         return "configure-readonly";
     }
     
+    public String getConfigContent(){
+        try{
+       URL url = Job.class.getResource("Job/configure.jelly");
+       InputStream input = url.openConnection().getInputStream();
+       ByteArrayOutputStream output = new ByteArrayOutputStream();
+       int b = 0;
+            while (b != -1) {
+                b = input.read();
+                if (b != -1) {
+                    output.write(b);
+                }
+            }
+            String outputConfig = output.toString();
+            return outputConfig.replace("it.EXTENDED_READ", "it.READ"); //change permission 
+        } catch (Exception e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.WARNING, "Read-only configuration plugin failed to load configuration script", e);
+            return null;
+        }
+    }
+    
     /**
     * Transformation of html code which modify all formular's items to read-only
     * 
@@ -58,7 +108,7 @@ public class JobConfiguration implements ProminentProjectAction {
             MetaClass c = WebApp.get(request.getServletContext()).getMetaClass(project.getClass());
             ByteArrayOutputStream out = new ByteArrayOutputStream(); 
             HTMLWriterOutput xmlOutput = HTMLWriterOutput.create(out);
-            Script script = c.loadTearOff(JellyClassTearOff.class).findScript("configure.jelly");            
+            Script script = compileScript();           
             xmlOutput.useHTML(true);          
             invoker.invokeScript(request, response, script, project, xmlOutput);
             String charset = Charset.defaultCharset().name();
